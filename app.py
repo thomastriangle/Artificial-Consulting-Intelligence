b02cb76739dffdeba2024dec167fd03c52c246c5
Theresa Ming <theresaming@gmail.com>
2018-12-23 19:51:55 +0800

    forms

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
-@app.route('/')
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+class CompanyForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+
+@app.route('/', methods=['GET', 'POST'])
 def home():  
-    return 'Hello, World!'
+    form = CompanyForm(request.form)
+    print form.errors
+    if request.method == 'POST':
+        name = request.form['name']
+        print name
+    if form.validate():
+        # Save the comment here.
+        flash('Hello ' + name)
+    else:
+        flash('All the form fields are required. ')
+ 
+    return render_template('hello.html', form=form)
+
+if __name__ == "__main__":
+    app.run()
